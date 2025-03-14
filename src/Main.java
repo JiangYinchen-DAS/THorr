1f5a0083f0ecde33e1332d5acdd5fcb900df518d
JiangYinchen-DAS <jiangyinchen00@gamil.com>
2025-03-14 21:08:55 +0800

    Nothing in it now, but something will in it in futrue.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {System.out.println("i = " + i);
+        }
+    }
+}
